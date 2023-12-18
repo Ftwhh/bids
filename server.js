ed6a20848254ebd912d8b0439f15d06baaa4028c
Ftwhh <109472232+Ftwhh@users.noreply.github.com>
2023-12-18 14:41:25 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,15 +3,27 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Basic in-memory database for demonstration
+// Simple in-memory storage for users and auctions
+const users = [];
 const auctions = [
-  { id: 1, title: 'Item 1', currentBid: 10 },
-  { id: 2, title: 'Item 2', currentBid: 20 },
+  { id: 1, title: 'Item 1', currentBid: 10, createdBy: 'user1' },
+  { id: 2, title: 'Item 2', currentBid: 20, createdBy: 'user2' },
 ];
 
 app.use(bodyParser.json());
 
 app.use(express.static('public'));
+
+// User authentication middleware
+app.use((req, res, next) => {
+  const { token } = req.headers;
+
+  if (!token || !users.some(user => user.token === token)) {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+
+  next();
+});
 
 app.get('/api/auctions', (req, res) => {
   res.json(auctions);
