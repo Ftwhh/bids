56b43c46824787476039594bb0bd0d8764c6c4ad
Ftwhh <109472232+Ftwhh@users.noreply.github.com>
2023-12-18 14:31:27 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
+document.addEventListener('DOMContentLoaded', async function () {
+  const auctionListContainer = document.getElementById('auctionList');
+  const auctions = await fetchAuctions();
+
+  auctions.forEach(auction => {
+    const auctionItem = createAuctionItem(auction);
+    auctionListContainer.appendChild(auctionItem);
+  });
+});
+
+async function fetchAuctions() {
+  const response = await fetch('https://api.example.com/auctions');
+  const auctions = await response.json();
+  return auctions;
+}
+
+function createAuctionItem(auction) {
+  const itemDiv = document.createElement('div');
+  itemDiv.innerHTML = `
+    <h3>${auction.title}</h3>
+    <p>Current Bid: $${auction.currentBid}</p>
+    <button onclick="placeBid(${auction.id})">Place Bid</button>
+  `;
+  return itemDiv;
+}
+
+async function placeBid(auctionId) {
+  // Implement bid logic here
+  // You may need to send a request to the backend to place a bid
+  console.log(`Placing bid for auction with ID ${auctionId}`);
+}
 
