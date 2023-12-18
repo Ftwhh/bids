221e4765601d2a3a43144540b9ca12054f193558
Ftwhh <109472232+Ftwhh@users.noreply.github.com>
2023-12-18 14:37:26 +0300

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', async function () {
 });
 
 async function fetchAuctions() {
-  const response = await fetch('https://api.example.com/auctions');
+  const response = await fetch('http://localhost:3000/api/auctions');
   const auctions = await response.json();
   return auctions;
 }
@@ -19,14 +19,27 @@ function createAuctionItem(auction) {
   itemDiv.innerHTML = `
     <h3>${auction.title}</h3>
     <p>Current Bid: $${auction.currentBid}</p>
+    <input type="number" placeholder="Enter bid amount" id="bidInput${auction.id}" />
     <button onclick="placeBid(${auction.id})">Place Bid</button>
   `;
   return itemDiv;
 }
 
 async function placeBid(auctionId) {
-  // Implement bid logic here
-  // You may need to send a request to the backend to place a bid
-  console.log(`Placing bid for auction with ID ${auctionId}`);
-}
+  const bidAmount = document.getElementById(`bidInput${auctionId}`).value;
+  const response = await fetch(`http://localhost:3000/api/place-bid/${auctionId}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ bidAmount: parseFloat(bidAmount) }),
+  });
 
+  const result = await response.json();
+  if (result.success) {
+    // Update the UI or take other actions as needed
+    console.log('Bid placed successfully');
+  } else {
+    console.error(result.error);
+  }
+}
